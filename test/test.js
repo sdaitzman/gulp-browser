f459bd5537b1330771224735efa29873a7873831
PhilKunz <github@philkunz.com>
2016-02-09 16:51:06 +0100

    update test

--- test/test.js
+++ test/test.js
@@ -8,7 +8,8 @@ var plugins = {
 describe("gulpBrowser", function () {
     describe(".browserify", function () {
         it("should run through smoothly", function (done) {
-            this.timeout(20000);
+            this.timeout(50000);
+            plugins.beautylog.info("Note: This takes longer then usual due to code coverage");
             plugins.gulp.task('gulpBrowserNormal', function (cb) {
                 var stream = plugins.gulp.src('./test/browserifyGulpTest.js')
                     .pipe(plugins.gulpBrowser.browserify())
